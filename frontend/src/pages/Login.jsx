9e1e99d4a6de19d85b91fea11456771d77f3e136
Priya Dharshini <pd568138@gmail.com>
2026-05-29 13:19:52 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -34,7 +34,7 @@ function Login() {
     try {
 
       const res = await fetch(
-        "http://localhost:5000/api/auth/login",
+        "https://skillbridge-project-1-ck1y.onrender.com/api/auth/login",
         {
           method: "POST",
           headers: {
