611e529b27f1ac5cc14b1a5e3519a22bc824add9
Priya Dharshini <pd568138@gmail.com>
2026-09-23 20:45:18 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://skillbridge-project-2.onrender.com/api/auth"
+  baseURL: "https://skillbridge-project-3.onrender.com//api/auth"
 });
 
 export const registerUser = (data) => API.post("/register", data);
