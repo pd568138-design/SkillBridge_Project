4fd843a2d0acdc36cd6e0b743ca414724093e682
Priya Dharshini <pd568138@gmail.com>
2026-09-23 22:46:20 +0530

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -34,7 +34,7 @@ function Dashboard() {
       try {
 
         const res = await fetch(
-          "https://skillbridge-project-2.onrender.com/api/challenges"
+          "https://skillbridge-project-3.onrender.com/api/challenges"
         );
 
         if(!res.ok){
